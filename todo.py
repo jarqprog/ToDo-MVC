58ef3a33efd7add214be4d8debdc50df3ecb72aa
jarqprog <jarqprog@gmail.com>
2017-10-26 00:59:33 +0200

    refactor

--- todo.py
+++ todo.py
@@ -4,8 +4,6 @@
 class ToDo():
     """Items ToDo class."""
 
-    created_items_counter = 0  # number of created ToDo instancess
-
     @classmethod
     def check_if_string_has_proper_length(cls, string, proper_length=20):
         """Return bool."""
@@ -28,19 +26,13 @@ class ToDo():
         name: string, max 20 characters
         description: string, max 150 characters
         is_done: bool, False by default
-        id: int
         """
         self.name = self.set_proper_length_of_the_string(name, proper_length=20)
         self.description = self.set_proper_length_of_the_string(description, proper_length=150)
         self.is_done = False
-        self.id = ToDo.created_items_counter
-        ToDo.created_items_counter += 1
 
     def __str__(self):
-        return "id: " + str(self.id) + ", name: " + self.name + "\n" + "description: " + self.description
-
-    # def remove_me(self):
-    #     __del__(self)
+        return "name: " + self.name + ", description: " + self.description
 
     def change_my_name(self, new_name):
         self.name = self.set_proper_length_of_the_string(new_name, proper_length=20)
@@ -50,13 +42,6 @@ class ToDo():
 
     def mark_me_as_done(self):
         self.is_done = True
-
-
-# Modify item
-# allow changing name
-# allow changing description
-# Delete item
-# Mark item as done
 
 
 task = ToDo("hjgfhajgsdasdashgdfashjgdfasdsahdgfgasdgfasdh12345", "opis")
