fd0f17f6089750d1d0922ab91710cdc15c9d718a
jarqprog <jarqprog@gmail.com>
2017-10-25 16:09:08 +0200

    add todolist class

---
+++ todolist.py
@@ -0,0 +1,8 @@
+"""Contains ToDoList class. In MVC structure it's Model."""
+
+from todo import ToDo
+
+
+class ToDoList():
+    """Items ToDo list class. Contains all tasks (ToDo objects) and methods to manage them."""
+    pass
